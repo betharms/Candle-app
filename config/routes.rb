038c26a1181c1db90bab5d15c61e8f54c4faf8e0
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-06 17:11:07 +0000

    admin controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :candles
+  resources :candles, only: [:index, :show]
   resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to: 'candles#index'
+    resources :candles, only: [:index, :new, :show, :edit, :update, :destroy]
+  end
+
 end
