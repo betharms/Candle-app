0ce59bfc99b25c7d6427a234b9615d5f1b86f035
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-12 12:30:57 +0000

    models and associations for now

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ApplicationRecord
+  has_many :users
   belongs_to :cart_candles
 end
--- app/models/cart_candle.rb
+++ app/models/cart_candle.rb
@@ -1,3 +1,4 @@
 class CartCandle < ApplicationRecord
+  has_many :carts
   belongs_to :candle, dependent: :destroy
 end
