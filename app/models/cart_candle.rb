1500416f313123af10ed4f38f38181910da76067
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-12 14:51:25 +0000

    removed 'amount' from cart candles

--- app/models/cart_candle.rb
+++ app/models/cart_candle.rb
@@ -1,4 +1,5 @@
 class CartCandle < ApplicationRecord
   belongs_to :candle
   belongs_to :cart
+  belongd_to :order
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_144132) do
+ActiveRecord::Schema.define(version: 2021_01_12_144830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2021_01_12_144132) do
   end
 
   create_table "cart_candles", force: :cascade do |t|
-    t.integer "amount"
     t.bigint "candle_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
