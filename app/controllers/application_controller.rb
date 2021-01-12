a61ef6be8766fb3a675c2996c9de3645c943f598
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-12 16:42:12 +0000

    application controller updated to include carts all over website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,32 @@
  class ApplicationController < ActionController::Base
+  before_action :current_cart
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def current_cart
+    if session[:cart_id]
+      cart = Cart.find_by(:id => session[:cart_id])
+      if cart.present?
+        @current_cart = cart
+      else
+        session[:cart_id] = nav-link
+      end
+    end
+
+    if session[:cart_id] == nil
+      @current_cart = Cart.create
+      session[:cart_id] = @current_cart.id
+    end
+  end
 
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :email, :password])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :email, :password])
   end
+
+
 
 end
 
