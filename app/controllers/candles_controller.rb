44c27910a79d62c316f46151c082af229e2a4f63
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-12 15:21:24 +0000

    candles controller updated

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -5,12 +5,41 @@ class CandlesController < ApplicationController
     @candle = Candle.all
   end
 
-  def show
-    @candle = Candle.find(params[:id])
+  def new
+    @candle = Candle.new
   end
 
-  def edit
+  def create
+    @candle = Candle.create(candle_params)
+    redirect_to candles_path
   end
+
+   def edit
+    @candle = Candle.find(params[:id])
+   end
+
+   def update
+    @candle = Candle.find(params[:id])
+    @candle.update(candle_params)
+    redirect_to candles_path
+   end
+
+   def destroy
+    @candle = Candle.find(params[:id])
+    @candle.destroy
+    redirect_to candles_path
+   end
+
+   private
+   def candle_params
+    params.require(:candle).permit(:name, :price)
+   end
+
+
+  # def show
+  #   @candle = Candle.find(params[:id])
+  # end
+
 end
 # a user has many carts
 # a cart has one user
