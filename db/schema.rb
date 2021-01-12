c0bb19abaca3bb6c7b66a5d3139270a243504496
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-12 12:19:51 +0000

    added cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_120752) do
+ActiveRecord::Schema.define(version: 2021_01_12_121843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2021_01_12_120752) do
     t.index ["candle_id"], name: "index_cart_candles_on_candle_id"
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.string "status"
+    t.bigint "cart_candles_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_candles_id"], name: "index_carts_on_cart_candles_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +69,5 @@ ActiveRecord::Schema.define(version: 2021_01_12_120752) do
   end
 
   add_foreign_key "cart_candles", "candles"
+  add_foreign_key "carts", "cart_candles", column: "cart_candles_id"
 end
