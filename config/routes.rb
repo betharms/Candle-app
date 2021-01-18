1821b86705d8d5c715c65a9a495d7100283ab926
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2021-01-18 14:06:49 +0000

    order controller fixed

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,11 @@ Rails.application.routes.draw do
   delete 'carts/:id', to: "carts#destroy"
 
   post 'cart_candles/:id/add', to: 'cart_candles#add_quantity', as: 'cart_candle_add'
-  post 'cart_candles/:id/redcue', to: 'cart_candles#reduce_quantity', as: 'cart_candle_reduce'
+  post 'cart_candles/:id/reduce', to: 'cart_candles#reduce_quantity', as: 'cart_candle_reduce'
   post 'cart_candles', to: 'cart_candle#create'
+
   get 'cart_candles/:id', to: "create_candles#show", as: "cart_candle"
   delete "cart_candles/:id", to: "cart_candles#destroy"
-  post 'cart_candles' => "cart_candles#create"
-  get 'cart_candles/:id' => "cart_candles#show", as: "line_item"
-  delete 'cart_candles/:id' => "cart_candles#destroy"
 
   resources :candles
   resources :orders
